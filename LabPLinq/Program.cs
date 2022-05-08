29f1306bf389f95c19f686e852220eec5da508d1
kerikir <vlasov-kerik@mail.ru>
2022-05-08 11:12:59 +0300

    Добавьте файлы проекта.

---
+++ LabPLinq/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LabPLinq
+{
+    internal class Program
+    {
+        static string[] GenerateArrayStringNumber(int size)
+        {
+            string[] arrayNumber = new string[size];
+            return arrayNumber;
+        }
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
